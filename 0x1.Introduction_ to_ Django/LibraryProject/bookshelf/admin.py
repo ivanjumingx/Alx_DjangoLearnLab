49991d0816adcf6efea4609151d4cdce9a9df1dc
Mingle <minglebiney@gmail.com>
2024-08-10 13:57:49 -0400

    CRUD Operations

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # List the fields to be displayed in the list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Add filters for easier navigation
+    list_filter = ('author', 'publication_year')
+
+    # Add a search bar to search through the title and author fields
+    search_fields = ('title', 'author')
 # Register your models here.
