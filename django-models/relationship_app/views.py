638b56b724256b71e1611201fdc45efaf1f66b47
Mingle <minglebiney@gmail.com>
2024-08-18 17:17:31 -0400

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,57 +1,19 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Book
 from .models import Library
-from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Function-Based Views
-
-@login_required
+# Function-Based View for Listing Books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-@login_required
-def library_detail(request, pk):
-    library = Library.objects.get(pk=pk)
-    return render(request, 'relationship_app/library_detail.html', {'library': library})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('list_books')
-    else:
-        form = UserLoginForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-def user_logout(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
-
-def user_register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-# Class-Based Views
-
-class LibraryDetailView(LoginRequiredMixin, DetailView):
+# Class-Based View for Displaying Library Details
+@method_decorator(login_required, name='dispatch')
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-    login_url = '/login/'  # Redirect to the login page if the user is not authenticated
