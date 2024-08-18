e5f8aa7f4579cde8daf266c8b16a15fd1c643f5c
Mingle <minglebiney@gmail.com>
2024-08-18 16:52:53 -0400

    Django Views and URL Configuration

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,3 +1,5 @@
+# relationship_app/models.py
+
 from django.db import models
 
 class Author(models.Model):
@@ -16,14 +18,7 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -12,3 +12,8 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
