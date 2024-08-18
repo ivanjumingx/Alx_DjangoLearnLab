3457d6d8f29714d55ea3a9962dc6d51a2cfce74f
Mingle <minglebiney@gmail.com>
2024-08-18 17:28:37 -0400

    Updated Script

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
