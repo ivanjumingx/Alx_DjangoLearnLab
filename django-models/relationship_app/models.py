aae2165a366a7639354713c6643dbb709a737568
Mingle <minglebiney@gmail.com>
2024-08-18 18:26:42 -0400

    Implementing User Authentication in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
@@ -30,3 +32,22 @@ class Librarian(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.phone_number}"
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Automatically create or update the UserProfile when a User is created or updated
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
