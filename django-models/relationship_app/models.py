dd252b1fe9973b4e10bcd9d6cb6cb2deb3f18781
Mingle <minglebiney@gmail.com>
2024-08-18 17:15:25 -0400

    Updates

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,24 +1,32 @@
-# relationship_app/models.py
-
 from django.db import models
+from django.contrib.auth.models import User
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    books = models.ManyToManyField('Book', related_name='libraries', blank=True)
+    librarian = models.OneToOneField('Librarian', on_delete=models.SET_NULL, null=True, blank=True, related_name='library')
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publication_year = models.PositiveIntegerField()
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
 
-class Library(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
+
+class Librarian(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.phone_number}"
